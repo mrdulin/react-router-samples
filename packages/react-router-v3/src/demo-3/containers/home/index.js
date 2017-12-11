f5316d820465e42e2bf2aea4a562527fc9e70182
mrdulin <novaline.dulin@gmail.com>
2017-12-11 20:59:58 +0800

    更新demo-3

--- packages/react-router-v3/src/demo-3/containers/home/index.js
+++ packages/react-router-v3/src/demo-3/containers/home/index.js
@@ -1,15 +1,43 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import LoadCardComponent from '../../components/Card';
 
-export default () => (
-  <div>
-    <h2>Home</h2>
+export default class extends React.Component {
 
-    <ul>
-      <li><Link to='/'>Home</Link></li>
-      <li><Link to='/about'>About</Link></li>
-      <li><Link to='/topics'>Topics</Link></li>
-      <li><Link to='/contact'>contact</Link></li>
-    </ul>
-  </div>
-)
+  constructor(...args) {
+    super(...args);
+    this.state = {
+      Card: null
+    };
+  }
+
+  componentWillMount() {
+    LoadCardComponent().then(({ Card }) => {
+      this.setState({ Card });
+    }).catch(e => {
+      const Card = <div>Card组件加载失败</div>
+      this.setState({ Card });
+    });
+  }
+
+  render() {
+    const { Card } = this.state;
+    return (
+      <div>
+        <h2>Home</h2>
+
+        <ul>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/about'>About</Link></li>
+          <li><Link to='/topics'>Topics</Link></li>
+          <li><Link to='/contact'>contact</Link></li>
+        </ul>
+
+        {
+          Card ? <Card /> :
+            <p>Card加载中</p>
+        }
+      </div>
+    );
+  }
+}
