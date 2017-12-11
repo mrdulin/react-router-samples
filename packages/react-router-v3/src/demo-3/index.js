32a0c2efff70ebc34d41733760f9feb83d8c2080
mrdulin <novaline.dulin@gmail.com>
2017-12-11 20:18:30 +0800

    添加demo-3，使用require.ensure的按需加载

---
+++ packages/react-router-v3/src/demo-3/index.js
@@ -0,0 +1,49 @@
+
+require("react-hot-loader/patch")
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
+
+import { AppContainer } from 'react-hot-loader';
+import App from './containers/app';
+import Loading from './containers/Loading';
+import { asyncComponent } from 'common/components/AsyncComponent/index-v2';
+
+import './style.css';
+
+const render = Component => {
+  ReactDOM.render(
+    <AppContainer>
+      <HashRouter>
+        <Component>
+          <Switch>
+            <Route exact path='/' component={asyncComponent({
+              loader: require('./containers/home/main').default,
+            })} />
+            <Route path='/about' component={asyncComponent({
+              loader: require('./containers/about/main').default
+            })} />
+            <Route path='/topics' component={asyncComponent({
+              loader: require('./containers/topics/main').default
+            })} />
+            <Route path='/contact' component={asyncComponent({
+              loader: require('./containers/contact/main').default,
+              loading: Loading
+            })} />
+            <Route path='/contact' component={asyncComponent({
+              loader: require('./containers/NoMatch/main').default,
+            })} />
+          </Switch>
+        </Component>
+      </HashRouter>
+    </AppContainer>,
+    document.getElementById('app')
+  );
+}
+
+render(App);
+
+if (module.hot && process.env.NODE_ENV !== 'production') {
+  module.hot.accept('./containers/app', () => { render(App) })
+}
