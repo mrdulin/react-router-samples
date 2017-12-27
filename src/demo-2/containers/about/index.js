7f285fe4c5695dcd4d085076546b4b1b05a934c4
mrdulin <novaline.dulin@gmail.com>
2017-12-27 20:10:43 +0800

    更新demo-2

--- src/demo-2/containers/about/index.js
+++ src/demo-2/containers/about/index.js
@@ -5,16 +5,13 @@ class About extends React.Component {
   render() {
     return (
       <div>
-        <h2>About Me v2</h2>
+        <h2>About Me</h2>
         <p>{this.props.aboutReducers.name}</p>
       </div>
     );
   }
 }
 
-export default connect(
-  function mapStateToProps({ aboutReducers }) {
-    return { aboutReducers };
-  }
-)(About);
-
+export default connect(function mapStateToProps({ aboutReducers }) {
+  return { aboutReducers };
+})(About);
